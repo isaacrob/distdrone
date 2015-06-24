8665c2a85757e1af0d5c52196556504638052c98
Isaac Robinson <isaacrob@me.com>
2015-06-24 20:17:57 +0000

    made it add picluster too. need to fix permissions in install

--- setup.py
+++ setup.py
@@ -14,5 +14,19 @@ setup(name="distdrone",
 	packages=["distdrone","distdrone.motion","distdrone.imgproc"],
 	scripts=["bin/trigger","bin/testpredictor","bin/centersearch","bin/installdronedeps"],
 	keywords=['drone','OpenCV','IPython','parallel'],
-	install_requires=['ipython==2.1','psutil'],
+	install_requires=['ipython==2.1','psutil>=3','paramiko>=1.15'],
 	zip_safe=False)
+
+#build picluster now
+from subprocess import call
+import os, socket
+
+#call(['sudo','tcpdump','-Z','pi'])
+os.chdir('/home/pi/.ipython')
+call(['rm','-rf','profile_picluster'])
+call(['git','clone','https://github.com/isaacrob/picluster','profile_picluster'])
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+s.connect(('8.8.8.8',80))
+myip=s.getsockname()[0]
+os.chdir('profile_picluster')
+call(['python','writeremotehosts.py','--controller_ip='+myip])
